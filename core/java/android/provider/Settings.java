7de41eb02a8f9afa0d363088f243aae99a0fe94c
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    Allow using 4G icon instead of LTE [1/2]
    Change-Id: Ieb912c64a60ddd42fe7b21d18dadea293ff159c5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6076,6 +6076,12 @@ public final class Settings {
         public static final String QS_FOOTER_TEXT_STRING = "qs_footer_text_string";
 
         /**
+         * Whether to display 4G icon instead LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
+
+        /**
          * These are all public system settings
          *
          * @hide
