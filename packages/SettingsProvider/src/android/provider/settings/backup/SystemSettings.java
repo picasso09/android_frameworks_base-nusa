bef11b439b651d8390cae2222562399a45a3e840
kldoc <fred.berbigier@gmail.com>
2019-10-10 22:56:28 +0200

    base: add Pulse and Ambient notification bars
    Add Pulse and Ambient Display bars for notifications
    
    idoybh (YAAP): Adapt to A12
    
    Change-Id: Ie4f1730bb15d0cd4db7ceb3a0281c114707d3789

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -95,5 +95,8 @@ public class SystemSettings {
         Settings.System.FLASHLIGHT_ON_CALL_RATE,
         Settings.System.NOTIFICATION_HEADERS,
         Settings.System.DOZE_ON_CHARGE,
+        Settings.System.PULSE_AMBIENT_LIGHT,
+        Settings.System.AMBIENT_NOTIFICATION_LIGHT_ENABLED,
+        Settings.System.AMBIENT_NOTIFICATION_LIGHT,
     };
 }
