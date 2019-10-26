0ccede59b1a54898fc900cbe1ebc589d35990df9
dmd79 <davidevinavil@gmail.com>
2019-10-27 00:35:29 +0200

    Notification light: Fix white color [3/3]
    Change-Id: Ic82eabd90c28e7312700e63d11c688a3736e79bb
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -7430,7 +7430,7 @@ public class NotificationManagerService extends SystemService {
     }
 
     private void forceShowLed(int color) {
-        if (color != -1) {
+        if (color != 0) {
             mNotificationLight.turnOff();
             mNotificationLight.setColor(color);
         } else {
@@ -7439,7 +7439,7 @@ public class NotificationManagerService extends SystemService {
     }
 
     private void forcePulseLed(int color, int onTime, int offTime) {
-        if (color != -1) {
+        if (color != 0) {
             mNotificationLight.turnOff();
             mNotificationLight.setFlashing(color, LogicalLight.LIGHT_FLASH_TIMED, onTime, offTime);
         } else {
