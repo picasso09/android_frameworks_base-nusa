e6b901d93d53d421d64b6b652c78c8b530439f3b
Anushek Prasal <anushekprasal@gmail.com>
2020-01-03 00:18:59 +0530

    base: Add Touch HIDL support
    All credits to LineageOS and related authors on:
    https://github.com/LineageOS/android_lineage-sdk
    
    Ported to AOSP from lineage-sdk by SKULSHADY
    
    [ anirudhgupta109 | AOSiP ]
     - Minimal import of LineageHardwareManager and HIDLHelper
     - We don't want LiveDisplay stuff
     - Nuke Legacy touchscreen support
    
    Change-Id: Ic8ca90663582c06f35562286f5ce5b2d84971ec4
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5975,6 +5975,12 @@ public final class Settings {
         public static final String STATUSBAR_NOTIF_COUNT = "statusbar_notif_count";
 
         /**
+         * Whether or not to vibrate when a touchscreen gesture is detected
+         * @hide
+         */
+        public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
+
+        /**
          * These are all public system settings
          *
          * @hide
