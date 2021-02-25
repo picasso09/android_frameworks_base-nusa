584054a6a4e8f8da6d61d3c187b652122e08e94c
Oliver Scott <olivercscott@gmail.com>
2021-02-25 11:29:35 -0500

    base: Wi-Fi timeout feature (1/2)
    SDB - adapt for A12 FLAG_MUTABLE
    
    Issue: calyxos#228
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -16075,6 +16075,12 @@ public final class Settings {
         public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
 
         /**
+         * The amount of time in milliseconds before wifi is turned off
+         * @hide
+         */
+        public static final String WIFI_OFF_TIMEOUT = "wifi_off_timeout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
