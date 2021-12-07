b883729af71d7e520d1ba5e7203b934ad1b3e5d1
PainKiller3 <ninadpatil100@gmail.com>
2021-12-07 19:19:19 +0100

    PixelPropsUtils: Update redfin fingerprint To December 2021 Security Patch
    Change-Id: I796678154b3384f079be768232a80f46a812942f

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -39,7 +39,7 @@ public class PixelPropsUtils {
             "com.google.android.googlequicksearchbox",
             "com.google.android.apps.recorder"
     };
-    
+
     private static final Map<String, Object> propsToChangePixelXL;
     private static final String[] packagesToChangePixelXL = {
             "com.google.android.apps.photos",
@@ -54,7 +54,6 @@ public class PixelPropsUtils {
             "com.samsung.android.neatplugin",
             "com.samsung.android.waterplugin"
     };
-    
 
     private static final Map<String, ArrayList<String>> propsToKeep;
     private static final String[] extraPackagesToChange = {
@@ -85,7 +84,7 @@ public class PixelPropsUtils {
         propsToChangePixel5.put("DEVICE", "redfin");
         propsToChangePixel5.put("PRODUCT", "redfin");
         propsToChangePixel5.put("MODEL", "Pixel 5");
-        propsToChangePixel5.put("FINGERPRINT", "google/redfin/redfin:12/SP1A.210812.015/7679548:user/release-keys");
+        propsToChangePixel5.put("FINGERPRINT", "google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys");
         propsToChangePixelXL = new HashMap<>();
         propsToChangePixelXL.put("BRAND", "google");
         propsToChangePixelXL.put("MANUFACTURER", "Google");
