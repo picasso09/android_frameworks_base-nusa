7787958cd7584a0c55a81f8e7cae4e520a0e6b8c
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: Integrate smartspace and backgesture provider
    Change-Id: Iadaaa3e24b3a30edeff59e0224d063bf71e20d3d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SystemUI/src/com/nusantara/systemui/dagger/NusantaraSysUIComponent.java
+++ packages/SystemUI/src/com/nusantara/systemui/dagger/NusantaraSysUIComponent.java
@@ -7,6 +7,9 @@ import com.android.systemui.dagger.SystemUIBinder;
 import com.android.systemui.dagger.SysUIComponent;
 import com.android.systemui.dagger.SystemUIModule;
 
+import com.nusantara.systemui.keyguard.NusantaraKeyguardSliceProvider;
+import com.nusantara.systemui.smartspace.KeyguardSmartspaceController;
+
 import dagger.Subcomponent;
 
 @SysUISingleton
@@ -22,4 +25,12 @@ public interface NusantaraSysUIComponent extends SysUIComponent {
     interface Builder extends SysUIComponent.Builder {
         NusantaraSysUIComponent build();
     }
+
+    /**
+     * Member injection into the supplied argument.
+     */
+    void inject(NusantaraKeyguardSliceProvider keyguardSliceProvider);
+
+    @SysUISingleton
+    KeyguardSmartspaceController createKeyguardSmartspaceController();
 }
