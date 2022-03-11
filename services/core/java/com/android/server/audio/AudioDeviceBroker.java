0c60443dda839aed14c0cf43a4cf750c4b288cde
Genkzsz11 <genkzsz11@gmail.com>
2022-03-11 11:23:17 +0700

    [SQUASED] Merge QTI BT
    Change-Id: Ie5c824a88b11cd7faf59770dc3af072de0ed6fc3

--- services/core/java/com/android/server/audio/AudioDeviceBroker.java
+++ services/core/java/com/android/server/audio/AudioDeviceBroker.java
@@ -538,7 +538,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
                 return true;
             }
             if (o instanceof BtDeviceConnectionInfo) {
-                return mDevice.equals(((BtDeviceConnectionInfo) o).mDevice);
+                return mDevice.equals(((BtDeviceConnectionInfo) o).mDevice) &&
+                       mProfile == (((BtDeviceConnectionInfo) o).mProfile);
             }
             return false;
         }
@@ -571,7 +572,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
             // when receiving a request to change the connection state of a device, this last
             // request is the source of truth, so cancel all previous requests that are already in
             // the handler
-            removeScheduledA2dpEvents(info.mDevice);
+            removeScheduledA2dpEvents(info.mDevice, info.mProfile);
 
             sendLMsgNoDelay(
                     info.mState == BluetoothProfile.STATE_CONNECTED
@@ -583,14 +584,14 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
     /** remove all previously scheduled connection and state change events for the given device */
     @GuardedBy("mDeviceStateLock")
-    private void removeScheduledA2dpEvents(@NonNull BluetoothDevice device) {
+    private void removeScheduledA2dpEvents(@NonNull BluetoothDevice device, int profile) {
         mBrokerHandler.removeEqualMessages(MSG_L_A2DP_DEVICE_CONFIG_CHANGE, device);
 
         final BtDeviceConnectionInfo connectionInfoToRemove = new BtDeviceConnectionInfo(device,
                 // the next parameters of the constructor will be ignored when finding the message
                 // to remove as the equality of the message's object is tested on the device itself
                 // (see BtDeviceConnectionInfo.equals() method override)
-                BluetoothProfile.STATE_CONNECTED, 0, false, -1);
+                BluetoothProfile.STATE_CONNECTED, profile, false, -1);
         mBrokerHandler.removeEqualMessages(MSG_L_A2DP_DEVICE_CONNECTION_CHANGE_EXT_DISCONNECTION,
                 connectionInfoToRemove);
         mBrokerHandler.removeEqualMessages(MSG_L_A2DP_DEVICE_CONNECTION_CHANGE_EXT_CONNECTION,
@@ -604,6 +605,18 @@ import java.util.concurrent.atomic.AtomicBoolean;
                 devInfoToRemove);
         mBrokerHandler.removeEqualMessages(MSG_L_A2DP_ACTIVE_DEVICE_CHANGE,
                 devInfoToRemove);
+    }
+
+    /*package*/ void postBluetoothA2dpDeviceConfigChangeExt(
+            @NonNull BluetoothDevice device,
+            @AudioService.BtProfileConnectionState int state, int profile,
+            boolean suppressNoisyIntent, int a2dpVolume) {
+         final BtDeviceConnectionInfo info = new BtDeviceConnectionInfo(device, state, profile,
+                 suppressNoisyIntent, a2dpVolume);
+         synchronized (mDeviceStateLock) {
+             removeScheduledA2dpEvents(info.mDevice, info.mProfile);
+             sendLMsgNoDelay(MSG_L_A2DP_ACTIVE_DEVICE_CHANGE_EXT, SENDMSG_QUEUE, info);
+         }
     }
 
     private static final class HearingAidDeviceConnectionInfo {
@@ -1415,6 +1428,22 @@ import java.util.concurrent.atomic.AtomicBoolean;
                     final int capturePreset = msg.arg1;
                     mDeviceInventory.onSaveClearPreferredDevicesForCapturePreset(capturePreset);
                 } break;
+                case MSG_L_A2DP_ACTIVE_DEVICE_CHANGE_EXT: {
+                    final BtDeviceConnectionInfo info = (BtDeviceConnectionInfo) msg.obj;
+                    AudioService.sDeviceLogger.log((new AudioEventLogger.StringEvent(
+                    "handleBluetoothA2dpActiveDeviceChangeExt "
+                           + " state=" + info.mState
+                           // only querying address as this is the only readily available
+                           // field on the device
+                           + " addr=" + info.mDevice.getAddress()
+                           + " prof=" + info.mProfile + " supprNoisy=" + info.mSupprNoisy
+                           + " vol=" + info.mVolume)).printLog(TAG));
+                    synchronized (mDeviceStateLock) {
+                        mDeviceInventory.handleBluetoothA2dpActiveDeviceChangeExt(
+                                info.mDevice, info.mState, info.mProfile,
+                                info.mSupprNoisy, info.mVolume);
+                    }
+                } break;
                 default:
                     Log.wtf(TAG, "Invalid message " + msg.what);
             }
@@ -1482,19 +1511,22 @@ import java.util.concurrent.atomic.AtomicBoolean;
     // process external command to (dis)connect a hearing aid device
     private static final int MSG_L_HEARING_AID_DEVICE_CONNECTION_CHANGE_EXT = 31;
 
-    private static final int MSG_IL_SAVE_PREF_DEVICES_FOR_STRATEGY = 32;
-    private static final int MSG_I_SAVE_REMOVE_PREF_DEVICES_FOR_STRATEGY = 33;
+    // process external command to (dis)connect or change active A2DP device
+    private static final int MSG_L_A2DP_ACTIVE_DEVICE_CHANGE_EXT = 32;
 
-    private static final int MSG_L_COMMUNICATION_ROUTE_CLIENT_DIED = 34;
-    private static final int MSG_CHECK_MUTE_MUSIC = 35;
-    private static final int MSG_REPORT_NEW_ROUTES_A2DP = 36;
+    private static final int MSG_IL_SAVE_PREF_DEVICES_FOR_STRATEGY = 33;
+    private static final int MSG_I_SAVE_REMOVE_PREF_DEVICES_FOR_STRATEGY = 34;
 
-    private static final int MSG_IL_SAVE_PREF_DEVICES_FOR_CAPTURE_PRESET = 37;
-    private static final int MSG_I_SAVE_CLEAR_PREF_DEVICES_FOR_CAPTURE_PRESET = 38;
+    private static final int MSG_L_COMMUNICATION_ROUTE_CLIENT_DIED = 35;
+    private static final int MSG_CHECK_MUTE_MUSIC = 36;
+    private static final int MSG_REPORT_NEW_ROUTES_A2DP = 37;
 
-    private static final int MSG_L_UPDATE_COMMUNICATION_ROUTE = 39;
-    private static final int MSG_IL_SET_PREF_DEVICES_FOR_STRATEGY = 40;
-    private static final int MSG_I_REMOVE_PREF_DEVICES_FOR_STRATEGY = 41;
+    private static final int MSG_IL_SAVE_PREF_DEVICES_FOR_CAPTURE_PRESET = 38;
+    private static final int MSG_I_SAVE_CLEAR_PREF_DEVICES_FOR_CAPTURE_PRESET = 39;
+
+    private static final int MSG_L_UPDATE_COMMUNICATION_ROUTE = 40;
+    private static final int MSG_IL_SET_PREF_DEVICES_FOR_STRATEGY = 41;
+    private static final int MSG_I_REMOVE_PREF_DEVICES_FOR_STRATEGY = 42;
 
     private static boolean isMessageHandledUnderWakelock(int msgId) {
         switch(msgId) {
@@ -1511,6 +1543,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
             case MSG_L_A2DP_DEVICE_CONNECTION_CHANGE_EXT_DISCONNECTION:
             case MSG_L_HEARING_AID_DEVICE_CONNECTION_CHANGE_EXT:
             case MSG_CHECK_MUTE_MUSIC:
+            case MSG_L_A2DP_ACTIVE_DEVICE_CHANGE_EXT:
                 return true;
             default:
                 return false;
