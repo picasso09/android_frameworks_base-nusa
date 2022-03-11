GrazieMaldini/du_frameworks_base

CustomDoze: fix custom gestures not working if default pulsing is disabled [1/2]
*we must be able to trigger ambient if pulse on notification
is disabled but we still want to use tilt, handwave or pocket mode

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -51,6 +51,7 @@ public class AmbientDisplayConfiguration {
                 || wakeScreenGestureEnabled(user)
                 || pickupGestureEnabled(user)
                 || tapGestureEnabled(user)
+                || pulseOnCustomDozeEventEnabled(user)
                 || doubleTapGestureEnabled(user);
     }
 
@@ -128,6 +129,13 @@ public class AmbientDisplayConfiguration {
     /** {@hide} */
     public String longPressSensorType() {
         return mContext.getResources().getString(R.string.config_dozeLongPressSensorType);
+    }
+
+    /** (@hide} */
+    private boolean pulseOnCustomDozeEventEnabled(int user) {
+        return (Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_POCKETMODE_GESTURE, 0) != 0
+                || Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_HANDWAVE_GESTURE, 0) != 0)
+                && pulseOnNotificationAvailable();
     }
 
     /** {@hide} */

picasso09/android_frameworks_base-nusa

Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -56,6 +56,7 @@ public class AmbientDisplayConfiguration {
                 || tapGestureEnabled(user)
                 || doubleTapGestureEnabled(user)
                 || quickPickupSensorEnabled(user)
+                || pulseOnCustomDozeEventEnabled(user)
                 || screenOffUdfpsEnabled(user);
     }
 
@@ -72,7 +73,9 @@ public class AmbientDisplayConfiguration {
 
     /** {@hide} */
     private boolean pulseOnCustomDozeEventEnabled(int user) {
-        return (Settings.System.getInt(mContext.getContentResolver(), Settings.System.DOZE_TRIGGER_DOUBLETAP, 0) != 0)
+        return (Settings.System.getInt(mContext.getContentResolver(), Settings.System.DOZE_TRIGGER_DOUBLETAP, 0) != 0
+                || Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_POCKETMODE_GESTURE, 0) != 0
+                || Settings.System.getInt(mContext.getContentResolver(), Settings.System.CUSTOM_AMBIENT_HANDWAVE_GESTURE, 0) != 0)
                 && pulseOnNotificationAvailable();
     }
 