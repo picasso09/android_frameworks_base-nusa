9c2701cffb4ff61a80f91f90b4daad70d39222be
Genkzsz11 <genkzsz11@gmail.com>
2022-04-04 23:50:39 +0700

    Revert "BatteryService: Add support for OEM fast charger detection"
    This reverts commit c4e9c3b75bc1b420057a39aa6807103f187f9d40.

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -178,9 +178,6 @@ public final class BatteryService extends SystemService {
 
     private boolean mBatteryLevelLow;
 
-    private boolean mOemFastCharger;
-    private boolean mLastOemFastCharger;
-
     private long mDischargeStartTime;
     private int mDischargeStartLevel;
 
@@ -625,8 +622,6 @@ public final class BatteryService extends SystemService {
         shutdownIfNoPowerLocked();
         shutdownIfOverTempLocked();
 
-        mOemFastCharger = isOemFastCharger();
-
         if (force || (mHealthInfo.batteryStatus != mLastBatteryStatus ||
                 mHealthInfo.batteryHealth != mLastBatteryHealth ||
                 mHealthInfo.batteryPresent != mLastBatteryPresent ||
@@ -637,8 +632,7 @@ public final class BatteryService extends SystemService {
                 mHealthInfo.maxChargingCurrent != mLastMaxChargingCurrent ||
                 mHealthInfo.maxChargingVoltage != mLastMaxChargingVoltage ||
                 mHealthInfo.batteryChargeCounter != mLastChargeCounter ||
-                mInvalidCharger != mLastInvalidCharger ||
-                mOemFastCharger != mLastOemFastCharger)) {
+                mInvalidCharger != mLastInvalidCharger)) {
 
             if (mPlugType != mLastPlugType) {
                 if (mLastPlugType == BATTERY_PLUGGED_NONE) {
@@ -809,7 +803,6 @@ public final class BatteryService extends SystemService {
             mLastChargeCounter = mHealthInfo.batteryChargeCounter;
             mLastBatteryLevelCritical = mBatteryLevelCritical;
             mLastInvalidCharger = mInvalidCharger;
-            mLastOemFastCharger = mOemFastCharger;
         }
     }
 
@@ -837,11 +830,9 @@ public final class BatteryService extends SystemService {
         intent.putExtra(BatteryManager.EXTRA_MAX_CHARGING_CURRENT, mHealthInfo.maxChargingCurrent);
         intent.putExtra(BatteryManager.EXTRA_MAX_CHARGING_VOLTAGE, mHealthInfo.maxChargingVoltage);
         intent.putExtra(BatteryManager.EXTRA_CHARGE_COUNTER, mHealthInfo.batteryChargeCounter);
-        intent.putExtra(BatteryManager.EXTRA_OEM_FAST_CHARGER, mOemFastCharger);
         if (DEBUG) {
             Slog.d(TAG, "Sending ACTION_BATTERY_CHANGED. scale:" + BATTERY_SCALE
-                    + ", info:" + mHealthInfo.toString()
-                    + ", mOemFastCharger:" + mOemFastCharger);
+                    + ", info:" + mHealthInfo.toString());
         }
 
         mHandler.post(() -> ActivityManager.broadcastStickyIntent(intent, UserHandle.USER_ALL));
@@ -891,25 +882,6 @@ public final class BatteryService extends SystemService {
         mContext.sendBroadcastAsUser(intent, UserHandle.ALL,
                 android.Manifest.permission.BATTERY_STATS);
         mLastBatteryLevelChangedSentMs = SystemClock.elapsedRealtime();
-    }
-
-    private boolean isOemFastCharger() {
-        final String path = mContext.getResources().getString(
-                com.android.internal.R.string.config_oemFastChargerStatusPath);
-
-        if (path.isEmpty())
-            return false;
-
-        final String value = mContext.getResources().getString(
-                com.android.internal.R.string.config_oemFastChargerStatusValue);
-
-        try {
-            return FileUtils.readTextFile(new File(path), value.length(), null).equals(value);
-        } catch (IOException e) {
-            Slog.e(TAG, "Failed to read oem fast charger status path: " + path);
-        }
-
-        return false;
     }
 
     // TODO: Current code doesn't work since "--unplugged" flag in BSS was purposefully removed.
